f8d4eaa421100db983c14ea94777f80d5cba6ca1
Kratos54901 <miranda.gabriel@estudante.ufjf.br>
2023-08-15 11:47:00 -0300

    teste

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 function loadChat() {
     var chatFrame = document.createElement('iframe');
-    script.src = 'https://kratos54901.github.io/';
-    document.body.appendChild(script);
+    chatFrame.src = 'https://kratos54901.github.io/';
+    document.body.appendChild(chatFrame);
 }
